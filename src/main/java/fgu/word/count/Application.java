bc2138402c299abec231df2628305327c7f849b3
Fernando Guerra <fnguerra@ebay.com>
2023-11-01 12:26:45 -0500

    Changed project name

--- src/main/java/fgu/word/count/Application.java
+++ src/main/java/fgu/word/count/Application.java
@@ -1,16 +1,13 @@
 package fgu.word.count;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Scanner;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
